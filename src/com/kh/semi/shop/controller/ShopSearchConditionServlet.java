01c707665464c32c6cacc8890770d930a5f57a4f
dogun <ash407@naver.com>
2020-02-11 21:42:04 +0900

    1

---
+++ src/com/kh/semi/shop/controller/ShopSearchConditionServlet.java
@@ -0,0 +1,56 @@
+package com.kh.semi.shop.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.semi.shop.model.service.ShopService;
+import com.kh.semi.shop.model.vo.Shop;
+
+/**
+ * Servlet implementation class ShopSearchConditionServlet
+ */
+@WebServlet("/SearchCondition.sc")
+public class ShopSearchConditionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShopSearchConditionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 인코딩
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json; charset=UTF-8");
+		
+		String tableType = request.getParameter("table");
+		String category = request.getParameter("category");
+		
+		System.out.println(tableType);
+		System.out.println(category);
+		
+		ArrayList<Shop> list = new ShopService().SeachCondition(tableType,category);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
