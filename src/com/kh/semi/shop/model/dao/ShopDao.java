c8af459bc9f1c885099645476e94f87a3d6a12b5
hyeonhee <hyeonhee9411@naver.com>
2020-02-11 19:19:07 +0900

    2020-02-11 (메인 검색기능 dao service 완료)

--- src/com/kh/semi/shop/model/dao/ShopDao.java
+++ src/com/kh/semi/shop/model/dao/ShopDao.java
@@ -1,5 +1,64 @@
 package com.kh.semi.shop.model.dao;
 
+import static com.kh.semi.common.JDBCTemplate.close;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import com.kh.semi.shop.model.vo.Shop;
+
 public class ShopDao {
+	
+	private Properties prop;
+
+	public ArrayList<Shop> searchMain(Connection con, String keyword) {
+		ArrayList<Shop> list = null;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		String sql = prop.getProperty("searchAddrMain");
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, keyword);
+			
+			rset = pstmt.executeQuery();
+			list = new ArrayList<Shop>();
+			
+			while(rset.next()) {
+				Shop s = new Shop();
+				
+				s.setShopPid(rset.getString("shopPid"));
+				s.setUserId(rset.getString("userId"));
+				s.setShopName(rset.getString("shopName"));
+				s.setShopImg(rset.getString("shopImg"));
+				s.setsAddr(rset.getString("sAddr"));
+				s.setsPhone(rset.getString("sPhone"));
+				s.setsInfo(rset.getString("sInfo"));
+				s.setOwnerId(rset.getString("ownerId"));
+				s.setsTime(rset.getDate("sTime"));
+				s.seteTime(rset.getDate("eTime"));
+				s.setShopDay(rset.getString("shopDay"));
+				s.setMenuCategory(rset.getString("menuCategory"));
+				s.setTableType(rset.getString("tableType"));
+				s.setAvgPay(rset.getInt("avgPay"));
+				s.setOutYn(rset.getString("outYn"));
+				
+				list.add(s);
+				
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return list;
+	}
   
 }
--- src/com/kh/semi/shop/model/service/ShopService.java
+++ src/com/kh/semi/shop/model/service/ShopService.java
@@ -1,12 +1,40 @@
 package com.kh.semi.shop.model.service;
 
+import static com.kh.semi.common.JDBCTemplate.getConnection;
+
+import java.sql.Connection;
 import java.util.ArrayList;
 
+import com.kh.semi.shop.model.dao.ShopDao;
 import com.kh.semi.shop.model.vo.Shop;
 
-public class ShopService {
+import static com.kh.semi.common.JDBCTemplate.*;
 
+public class ShopService {
+	
+	private ShopDao sDao = new ShopDao();
+
+	/**
+	 * 메인 페이지 검색
+	 * @param keyword
+	 * @return
+	 */
 	public ArrayList<Shop> searchMain(String keyword) {
+		Connection con = getConnection();
+		
+		ArrayList<Shop> list = null;
+		
+		if(keyword.length() > 0) {
+			list = sDao.searchMain(con,keyword);
+			
+		}else {
+			
+			// 모든 shop리스트 조회
+//			list = sDao.selectList(con);
+		}
+		
+		close(con);
+		
 		return null;
 	}
  
