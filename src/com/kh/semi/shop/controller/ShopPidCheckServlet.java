aca997ed3ec649d09eb40064de95284666beb8dc
Gianna <Gianna@DESKTOP-3EJC05V>
2020-02-21 21:52:11 +0900

    commit

--- src/com/kh/semi/shop/controller/ShopPidCheckServlet.java
+++ src/com/kh/semi/shop/controller/ShopPidCheckServlet.java
@@ -1,7 +1,10 @@
 package com.kh.semi.shop.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -32,15 +35,38 @@ public class ShopPidCheckServlet extends HttpServlet {
 		String searchPid = request.getParameter("Shoppid"); 
 		String searchName = request.getParameter("Shopname");
 		
-	String address = "http://apis.data.go.kr/B552015/NpsBplcInfoInqireService/getBassInfoSearch?serviceKey=J7gW4zUA9N1YP74mfR%2BCv9bRE1jBZsjxpJoj1ASuF2ncC3aO2Yu7U7uirQTKxcGtSx5aV2WKmdZfAAO8hDHP6w%3D%3D&wkpl_nm="
+	 String address = "http://apis.data.go.kr/B552015/NpsBplcInfoInqireService/getBassInfoSearch?serviceKey=J7gW4zUA9N1YP74mfR%2BCv9bRE1jBZsjxpJoj1ASuF2ncC3aO2Yu7U7uirQTKxcGtSx5aV2WKmdZfAAO8hDHP6w%3D%3D&wkpl_nm="
 			+ searchName + "&bzowr_rgst_no=" + searchPid;
 		
 	System.out.println(searchPid);
 	System.out.println(searchName);
 	System.out.println(address);
 	
-	
 	}
+	
+	/*------------------------------------------------------------------------------------
+	
+	  
+	        BufferedReader br = null;
+	        try{            
+	        	String address = "http://apis.data.go.kr/B552015/NpsBplcInfoInqireService/getBassInfoSearch?serviceKey=J7gW4zUA9N1YP74mfR%2BCv9bRE1jBZsjxpJoj1ASuF2ncC3aO2Yu7U7uirQTKxcGtSx5aV2WKmdZfAAO8hDHP6w%3D%3D&wkpl_nm="
+	        			+ searchName + "&bzowr_rgst_no=" + searchPid;
+	            URL url = new URL(address);
+	            HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
+	            urlconnection.setRequestMethod("GET");
+	            br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(),"UTF-8"));
+	           String result = "";
+	             String line;
+	            while((line = br.readLine()) != null) {
+	                result = result + line + "\n";
+	            }
+	            System.out.println(result);
+	        }catch(Exception e){
+	            System.out.println(e.getMessage());
+	        }
+	    }
+*/
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
--- web/views/ShopPidCheck.jsp
+++ web/views/ShopPidCheck.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
+    
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,12 +10,36 @@
 <title>사업자 번호 조회</title>
 </head>
 <body>
+
 <div>
 <form action="/siktam/spc.me" method="post">
-사업자번호 : <input type="text" class="form-control" name="Shoppid">
-사업장명 :<input type="text" class="form-control" name="Shopname">
-                    <button id="shopNum">확인</button>
+사업자번호 : <input type="text" class="form-control" name="Shoppid" id="Shoppid">
+사업장명 :<input type="text" class="form-control" name="Shopname" id="Shopname">
+      <input type="button" id="shopNumChk" value="사업자번호 체크"> 
+</form>
+
+<form>
+	<form action="registerCompany_2.jsp" method="post">
+    <input type="button" id="shopPidInput" value="입력하기">
+
+</form>                    
+                    
+ <!-- 사업자번호 체크 버튼 누른후 사업자번호 확인되어야 입력하기 버튼 활성화 되도록 -->                   
 </form>
 </div>
+
+<script>
+
+/* $("#shopPidInput")click.function(){
+	$.ajax({
+		url : "registerCompany_2.jsp",
+		type : "get",
+		date : {
+			
+		}
+	}) */
+	
+	
+</script>
 </body>
 </html>
