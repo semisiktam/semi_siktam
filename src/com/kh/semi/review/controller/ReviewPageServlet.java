f23953f5238144bf424cdb3d694e7d9752fd53c0
user2 <user2@DESKTOP-JSOJQ81>
2020-02-21 16:58:13 +0900

    202002121

--- src/com/kh/semi/review/controller/ReviewPageServlet.java
+++ src/com/kh/semi/review/controller/ReviewPageServlet.java
@@ -34,6 +34,7 @@ public class ReviewPageServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		// 리뷰 리스트 받아오기 최신순
 		// 멤버(로그인정보)(헤더에 딸려온다) 와 가게 번호 받아오기 -> 가게 번호로 가게 정보 가져오는 기능
 		
